eb69f1d697a6840e487140ade6b3e69024fb689a
prAkash-SVMX <prakashnarayan.shukla@servicemax.com>
2021-11-21 12:25:49 +0530

    Added email validation and password validation in RegisterPage.java file

--- app/src/main/java/com/example/schoolmanagement2/RegisterPage.java
+++ app/src/main/java/com/example/schoolmanagement2/RegisterPage.java
@@ -52,6 +52,7 @@ public class RegisterPage extends AppCompatActivity {
                 String Email = edit3.getText().toString();
                 String Password = edit4.getText().toString();
                 String Conform = edit5.getText().toString();
+                String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
                 final String[] gender = new String[1];
                  if(FullName.equals("") || UserName.equals("") || Email.equals("") || Password.equals("") || Conform.equals(""))
                  {
@@ -59,28 +60,35 @@ public class RegisterPage extends AppCompatActivity {
                  }
                  else
                  {
-                     mAuth.createUserWithEmailAndPassword(Email, Password)
-                             .addOnCompleteListener(RegisterPage.this, new OnCompleteListener<AuthResult>() {
-                                 @Override
-                                 public void onComplete(@NonNull Task<AuthResult> task) {
-                                     if (task.isSuccessful()) {
-                                         // Sign in success, update UI with the signed-in user's information
-                                         Log.d(" Firebase-User ", "createUserWithEmail:success");
-                                         FirebaseUser user = mAuth.getCurrentUser();
-                                       //  updateUI(user);
-                                         Toast.makeText(RegisterPage.this,"user "+user,Toast.LENGTH_LONG).show();
-                                     } else {
-                                         // If sign in fails, display a message to the user.
-                                         Log.w(" Firebase-User ", "createUserWithEmail:failure", task.getException());
-                                         Toast.makeText(RegisterPage.this, "Authentication failed.",
-                                                 Toast.LENGTH_SHORT).show();
-                                       //  updateUI(null);
+                     if(Email.matches(emailPattern) && Password.length()>=6){
+                         mAuth.createUserWithEmailAndPassword(Email, Password)
+                                 .addOnCompleteListener(RegisterPage.this, new OnCompleteListener<AuthResult>() {
+                                     @Override
+                                     public void onComplete(@NonNull Task<AuthResult> task) {
+                                         if (task.isSuccessful()) {
+                                             // Sign in success, update UI with the signed-in user's information
+                                             Log.d(" Firebase-User ", "createUserWithEmail:success");
+                                             FirebaseUser user = mAuth.getCurrentUser();
+                                             //  updateUI(user);
+                                             Toast.makeText(getApplicationContext(),"You Succussfully Registed ",Toast.LENGTH_SHORT).show();
+                                             Intent intent = new Intent(getApplicationContext(),loginpage.class);
+                                             startActivity(intent);
+                                             Toast.makeText(RegisterPage.this,"user "+user,Toast.LENGTH_LONG).show();
+                                         } else {
+                                             // If sign in fails, display a message to the user.
+                                             Log.w(" Firebase-User ", "createUserWithEmail:failure", task.getException());
+                                             Toast.makeText(RegisterPage.this, "Authentication failed.",
+                                                     Toast.LENGTH_SHORT).show();
+                                             //  updateUI(null);
+                                         }
                                      }
-                                 }
-                             });
-                     Toast.makeText(getApplicationContext(),"You Succussfully Registed ",Toast.LENGTH_SHORT).show();
-                     Intent intent = new Intent(getApplicationContext(),loginpage.class);
-                     startActivity(intent);
+                                 });
+                     }
+                     else {
+                         Toast.makeText(getApplicationContext(),"Invalid email address",Toast.LENGTH_SHORT).show();
+                     }
+
+
                  }
                 radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
                     @Override
